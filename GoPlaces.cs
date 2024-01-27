c70ef9b3398c864e5fb0e1d266e1e83e55325a5f
undertayker <andrey.kozlovskiy99@mail.ru>
2024-01-27 19:13:31 +0300

    Update GoPlaces.cs

--- GoPlaces.cs
+++ GoPlaces.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Transform))]
-public class Move : MonoBehaviour
+public class Movement : MonoBehaviour
 {
    [SerializeField] private float _speed;
    [SerializeField] private Transform _waypoints;
